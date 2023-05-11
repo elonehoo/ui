30bd4c0d0c2df01bc41f3610294c2f25a2d27803
elonehoo <hi@elonehoo.me>
2023-05-11 17:37:56 +0800

    feat: add more button class

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,25 @@ import {
   avatarGroup,
   button,
   buttonDefault,
-  buttonDefaultActive
+  buttonDefaultActive,
+  buttonPrimary,
+  buttonPrimaryActive,
+  buttonSecondary,
+  buttonSecondaryActive,
+  buttonAccent,
+  buttonAccentActive,
+  buttonInfo,
+  buttonInfoActive,
+  buttonSuccess,
+  buttonSuccessActive,
+  buttonWarning,
+  buttonWarningActive,
+  buttonError,
+  buttonErrorActive,
+  buttonGhost,
+  buttonGhostActive,
+  buttonLink,
+  buttonLinkActive
 } from './styled'
 import {Colors} from './color/theme'
 
@@ -33,9 +51,57 @@ export function Samuui(): Preset {
       [button.name,button.value],
       [buttonDefault.name,buttonDefault.value],
       [buttonDefaultActive.name,buttonDefaultActive.value],
+      [buttonPrimary.name,buttonPrimary.value],
+      [buttonPrimaryActive.name,buttonPrimaryActive.value],
+      [buttonSecondary.name,buttonSecondary.value],
+      [buttonSecondaryActive.name,buttonSecondaryActive.value],
+      [buttonAccent.name,buttonAccent.value],
+      [buttonAccentActive.name,buttonAccentActive.value],
+      [buttonInfo.name,buttonInfo.value],
+      [buttonInfoActive.name,buttonInfoActive.value],
+      [buttonSuccess.name,buttonSuccess.value],
+      [buttonSuccessActive.name,buttonSuccessActive.value],
+      [buttonWarning.name,buttonWarning.value],
+      [buttonWarningActive.name,buttonWarningActive.value],
+      [buttonError.name,buttonError.value],
+      [buttonErrorActive.name,buttonErrorActive.value],
+      [buttonGhost.name,buttonGhost.value],
+      [buttonGhostActive.name,buttonGhostActive.value],
+      [buttonLink.name,buttonLink.value],
+      [buttonLinkActive.name,buttonLinkActive.value],
     ],
     preflights:[
-
+      {
+        getCSS:() =>{
+          return `
+            @keyframes button-pop {
+              0% {
+                transform: scale(0.95);
+              }
+              40% {
+                transform: scale(1.02);
+              }
+              100% {
+                transform: scale(1);
+              }
+            }
+          `
+        }
+      },
+      {
+        getCSS:()=>{
+          return `
+            @keyframes spin {
+              from {
+                transform: rotate(0deg);
+              }
+              to {
+                transform: rotate(360deg);
+              }
+            }
+          `
+        }
+      }
     ]
   }
 }
--- src/styled/button.ts
+++ src/styled/button.ts
@@ -21,3 +21,120 @@ export const buttonDefaultActive:ShortcutsType = {
   name:'btn-default-active',
   value: "border-light-neutral-focus dark:border-dark-neutral-focus bg-light-neutral-focus dark:bg-dark-neutral-focus"
 }
+
+export const buttonPrimary:ShortcutsType = {
+  name:'btn-primary',
+  value:
+    "border-light-primary dark:border-dark-primary bg-light-primary dark:bg-dark-primary text-light-primary-content dark:text-dark-primary-content " +
+    "hover:border-light-primary-focus dark:hover:border-dark-primary-focus hover:bg-light-primary-focus dark:hover:bg-dark-primary-focus " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-#661AE6 outline-offset-2"
+}
+
+export const buttonPrimaryActive:ShortcutsType = {
+  name:'btn-primary-active',
+  value: "border-light-primary-focus dark:border-dark-primary-focus bg-light-primary-focus dark:bg-dark-primary-focus"
+}
+
+export const buttonSecondary:ShortcutsType = {
+  name:'btn-secondary',
+  value:
+    "border-light-secondary dark:border-dark-secondary bg-light-secondary dark:bg-dark-secondary text-light-secondary-content dark:text-dark-secondary-content " +
+    "hover:border-light-secondary-focus dark:hover:border-dark-secondary-focus hover:bg-light-secondary-focus dark:hover:bg-dark-secondary-focus " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-#003320 outline-offset-2"
+}
+
+export const buttonSecondaryActive:ShortcutsType = {
+  name:'btn-secondary-active',
+  value: "border-light-secondary-focus dark:border-dark-secondary-focus bg-light-secondary-focus dark:bg-dark-secondary-focus"
+}
+
+export const buttonAccent:ShortcutsType = {
+  name:'btn-accent',
+  value:
+    "border-light-accent dark:border-dark-accent bg-light-accent dark:bg-dark-accent text-light-accent-content dark:text-dark-accent-content " +
+    "hover:border-light-accent-focus dark:hover:border-dark-accent-focus hover:bg-light-accent-focus dark:hover:bg-dark-accent-focus " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-#1FB2A5 outline-offset-2"
+}
+
+export const buttonAccentActive:ShortcutsType = {
+  name:'btn-accent-active',
+  value: "border-light-accent-focus dark:border-dark-accent-focus bg-light-accent-focus dark:bg-dark-accent-focus"
+}
+
+export const buttonInfo:ShortcutsType = {
+  name:'btn-info',
+  value:
+    "border-light-info dark:border-dark-info bg-light-info dark:bg-dark-info text-light-info-content dark:text-dark-info-content " +
+    "hover:border-light-info dark:hover:border-dark-info hover:bg-light-info dark:hover:bg-dark-info " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-#3ABFF8 outline-offset-2"
+}
+
+export const buttonInfoActive:ShortcutsType = {
+  name:'btn-info-active',
+  value: "border-light-info dark:border-dark-info bg-light-info dark:bg-dark-info"
+}
+
+export const buttonSuccess:ShortcutsType = {
+  name:'btn-success',
+  value:
+    "border-light-success dark:border-dark-success bg-light-success dark:bg-dark-success text-light-success-content dark:text-dark-success-content " +
+    "hover:border-light-success dark:hover:border-dark-success hover:bg-light-success dark:hover:bg-dark-success " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-#36D399 outline-offset-2"
+}
+
+export const buttonSuccessActive:ShortcutsType = {
+  name:'btn-success-active',
+  value: "border-light-success dark:border-dark-success bg-light-success dark:bg-dark-success"
+}
+
+export const buttonWarning:ShortcutsType = {
+  name:'btn-warning',
+  value:
+    "border-light-warning dark:border-dark-warning bg-light-warning dark:bg-dark-warning text-light-warning-content dark:text-dark-warning-content " +
+    "hover:border-light-warning dark:hover:border-dark-warning hover:bg-light-warning dark:hover:bg-dark-warning " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-#FBBD23 outline-offset-2"
+}
+
+export const buttonWarningActive:ShortcutsType = {
+  name:'btn-warning-active',
+  value: "border-light-warning dark:border-dark-warning bg-light-warning dark:bg-dark-warning"
+}
+
+export const buttonError:ShortcutsType = {
+  name:'btn-error',
+  value:
+    "border-light-error dark:border-dark-error bg-light-error dark:bg-dark-error text-light-error-content dark:text-dark-error-content " +
+    "hover:border-light-error dark:hover:border-dark-error hover:bg-light-error dark:hover:bg-dark-error " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-#FBBD23 outline-offset-2"
+}
+
+export const buttonErrorActive:ShortcutsType = {
+  name:'btn-error-active',
+  value: "border-light-error dark:border-dark-error bg-light-error dark:bg-dark-error "
+}
+
+export const buttonGhost:ShortcutsType = {
+  name:'btn-ghost',
+  value:
+    "border border-transparent bg-transparent text-current " +
+    "hover:border-opacity-0 hover:bg-light-base-content dark:hover:bg-dark-base-content hover:bg-opacity-20 " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-current outline-offset-2"
+}
+
+export const buttonGhostActive:ShortcutsType = {
+  name:'btn-ghost-active',
+  value: "border-opacity-0 bg-light-base-content dark:bg-dark-base-content bg-opacity-20"
+}
+
+export const buttonLink:ShortcutsType = {
+  name:'btn-link',
+  value:
+    "border-transparent bg-transparent text-light-primary dark:text-dark-primary underline " +
+    "hover:border-transparent hover:bg-transparent hover:underline " +
+    "focus-visible:outline-2px focus-visible:outline-solid focus-visible:outline-current outline-offset-2"
+}
+
+export const buttonLinkActive:ShortcutsType = {
+  name:'btn-link-active',
+  value: "border-transparent bg-transparent underline"
+}
