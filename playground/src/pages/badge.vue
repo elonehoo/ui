71183c59536c705fd5d504b1b21a906ca19a1d1c
elonehoo <hi@elonehoo.me>
2023-05-17 14:30:46 +0800

    feat: add example badge

--- playground/src/pages/badge.vue
+++ playground/src/pages/badge.vue
@@ -39,4 +39,50 @@
     <div class="badge badge-sm ml-1"></div>
     <div class="badge badge-xs ml-1"></div>
   </div>
+  <div m="t-5">
+    <div class="badge badge-info gap-2">
+      <div class="i-carbon-close  inline-block w-4 h-4 stroke-current"></div>
+      info
+    </div>
+    <div class="badge badge-success gap-2 ml-1">
+      <div class="i-carbon-close  inline-block w-4 h-4 stroke-current"></div>
+      success
+    </div>
+    <div class="badge badge-warning gap-2 ml-1">
+      <div class="i-carbon-close  inline-block w-4 h-4 stroke-current"></div>
+      warning
+    </div>
+    <div class="badge badge-error gap-2 ml-1">
+      <div class="i-carbon-close  inline-block w-4 h-4 stroke-current"></div>
+      error
+    </div>
+  </div>
+  <div m="t-5">
+    <h2 class="text-xl">
+      Heading
+      <span class="badge badge-lg">NEW</span>
+    </h2>
+    <h3 class="text-lg">
+      Heading
+      <span class="badge badge-md">NEW</span>
+    </h3>
+    <h4 class="text-base">
+      Heading
+      <span class="badge badge-sm">NEW</span>
+    </h4>
+    <h5 class="text-sm">
+      Heading
+      <span class="badge badge-xs">NEW</span>
+    </h5>
+  </div>
+  <div m="t-5">
+    <button class="btn gap-2">
+      Inbox
+      <div class="badge">+99</div>
+    </button>
+    <button class="btn gap-2 ml-1">
+      Inbox
+      <div class="badge badge-secondary">+99</div>
+    </button>
+  </div>
 </template>
