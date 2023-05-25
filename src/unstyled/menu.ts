a1e7dd9cd2a445a13fe2df0b4a3ae5310feef4a6
elonehoo <hi@elonehoo.me>
2023-05-25 15:23:20 +0800

    fix: update menu unstyled

--- src/unstyled/menu.ts
+++ src/unstyled/menu.ts
@@ -22,50 +22,50 @@ export function menuUnStyled(
           }
         }
       }
-      :where(.menu li) {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        flex-wrap: wrap;
-        align-items: stretch;
-        flex-shrink: 0;
-      }
-      .menu :where(li:not(.menu-title)) > :where(*:not(ul)) {
-        display: flex;
-      }
-      .menu :where(li:not(.disabled):not(.menu-title)) > :where(*:not(ul)) {
-        cursor: pointer;
-        -webkit-user-select: none;
-        user-select: none;
-        align-items: center;
-        outline: 2px solid transparent;
-        outline-offset: 2px;
-      }
-      .menu > :where(li > *:not(ul):focus) {
-        outline: 2px solid transparent;
-        outline-offset: 2px;
-      }
-      .menu > :where(li.disabled > *:not(ul):focus) {
-        cursor: auto;
-      }
+    }
+    :where(.menu li) {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      align-items: stretch;
+      flex-shrink: 0;
+    }
+    .menu :where(li:not(.menu-title)) > :where(*:not(ul)) {
+      display: flex;
+    }
+    .menu :where(li:not(.disabled):not(.menu-title)) > :where(*:not(ul)) {
+      cursor: pointer;
+      -webkit-user-select: none;
+      user-select: none;
+      align-items: center;
+      outline: 2px solid transparent;
+      outline-offset: 2px;
+    }
+    .menu > :where(li > *:not(ul):focus) {
+      outline: 2px solid transparent;
+      outline-offset: 2px;
+    }
+    .menu > :where(li.disabled > *:not(ul):focus) {
+      cursor: auto;
+    }
 
-      .menu > :where(li) :where(ul) {
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-      }
-      .menu > :where(li) > :where(ul) {
-        display: none;
-        position: absolute;
-        top: initial;
-        left: 100%;
-      }
-      .menu > :where(li:hover) > :where(ul) {
-        display: flex;
-      }
-      .menu > :where(li:focus) > :where(ul) {
-        display: flex;
-      }
+    .menu > :where(li) :where(ul) {
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+    }
+    .menu > :where(li) > :where(ul) {
+      display: none;
+      position: absolute;
+      top: initial;
+      left: 100%;
+    }
+    .menu > :where(li:hover) > :where(ul) {
+      display: flex;
+    }
+    .menu > :where(li:focus) > :where(ul) {
+      display: flex;
     }
     &[class*=" px-"]:not(&[class*=" px-0"]) li > *,
     &[class^="px-"]:not(&[class^="px-0"]) li > *,
@@ -104,12 +104,12 @@ export function menuUnStyled(
     :where(li:not(.menu-title):not(:empty)) > :where(:not(ul).active),
     :where(li:not(.menu-title):not(:empty)) > :where(*:not(ul):active) {
       background-color: ${colors.light.primary};
-      color: ${colors.light.primary.content};
+      color: ${colors.light.primaryContent};
     }
-    .dark :where(li:not(.menu-title):not(:empty)) > :where(:not(ul).active),
+    .dark:where(li:not(.menu-title):not(:empty)) > :where(:not(ul).active),
     :where(li:not(.menu-title):not(:empty)) > :where(*:not(ul):active) {
       background-color: ${colors.dark.primary};
-      color: ${colors.dark.primary.content};
+      color: ${colors.dark.primaryContent};
     }
     :where(li:empty) {
       margin-left: 1rem;
@@ -155,9 +155,11 @@ export function menuUnStyled(
       }
       hover:a {
         border-color: ${colors.light.primary};
+        color: ${colors.light.primaryContent};
       }
       .dark hover:a {
         border-color: ${colors.dark.primary};
+        color: ${colors.dark.primaryContent};
       }
     }
     &.compact {
@@ -284,6 +286,100 @@ export function menuUnStyled(
     border-top-right-radius: inherit;
     border-bottom-right-radius: inherit;
     border-bottom-left-radius: inherit;
+  }
+  .menu-vertical :where(li.bordered > *) {
+    border-bottom-width: 0;
+    border-left-width: 4px;
+  }
+  .menu-horizontal :where(li.bordered > *) {
+    border-bottom-width: 4px;
+    border-left-width: 0;
+  }
+  .menu-normal :where(li > *) {
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5rem;
+  }
+  .menu-compact :where(li > *) {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+  }
+  .menu-vertical > :where(li:first-child) {
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+    border-bottom-right-radius: unset;
+    border-bottom-left-radius: unset;
+  }
+  .menu-vertical > :where(li:first-child) > :where(*:not(ul)) {
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+    border-bottom-right-radius: unset;
+    border-bottom-left-radius: unset;
+  }
+  .menu-vertical > :where(li:last-child) {
+    border-top-left-radius: unset;
+    border-top-right-radius: unset;
+    border-bottom-right-radius: inherit;
+    border-bottom-left-radius: inherit;
+  }
+  .menu-vertical > :where(li:last-child) > :where(*:not(ul)) {
+    border-top-left-radius: unset;
+    border-top-right-radius: unset;
+    border-bottom-right-radius: inherit;
+    border-bottom-left-radius: inherit;
+  }
+
+  .menu-horizontal > :where(li:first-child) {
+    border-top-left-radius: inherit;
+    border-top-right-radius: unset;
+    border-bottom-right-radius: unset;
+    border-bottom-left-radius: inherit;
+  }
+  .menu-horizontal > :where(li:first-child) > :where(*:not(ul)) {
+    border-top-left-radius: inherit;
+    border-top-right-radius: unset;
+    border-bottom-right-radius: unset;
+    border-bottom-left-radius: inherit;
+  }
+  .menu-horizontal > :where(li:last-child) {
+    border-top-left-radius: unset;
+    border-top-right-radius: inherit;
+    border-bottom-right-radius: inherit;
+    border-bottom-left-radius: unset;
+  }
+  .menu-horizontal > :where(li:last-child) > :where(*:not(ul)) {
+    border-top-left-radius: unset;
+    border-top-right-radius: inherit;
+    border-bottom-right-radius: inherit;
+    border-bottom-left-radius: unset;
+  }
+  .menu-vertical {
+    flex-direction: column;
+    :where(li) {
+      flex-direction: column;
+    }
+  }
+
+  .menu-vertical > :where(li) > :where(ul) {
+    top: initial;
+    left: 100%;
+  }
+
+  .menu-horizontal {
+    display: inline-flex;
+    flex-direction: row;
+    width: max-content;
+    :where(li) {
+      flex-direction: row;
+    }
+  }
+
+  .menu-horizontal > :where(li) > :where(ul) {
+    top: 100%;
+    left: initial;
   }
   `
 }
