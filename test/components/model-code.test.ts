94ee7f7a310e68d383963a9040e25fee0df2b628
elonehoo <hi@elonehoo.me>
2023-07-23 20:45:14 +0800

    test: add model code

---
+++ test/components/model-code.test.ts
@@ -0,0 +1,44 @@
+import { describe,it,expect } from 'vitest'
+import generator from '../setup'
+import { prettierCSS,uiColor, hexToRgb } from '../utils'
+
+describe('model code color',()=>{
+  for(const color of uiColor){
+    it(`model code ${color}`, async ()=>{
+      const { getLayer } = await generator.generate(`model-code-${color}`)
+      const css = await prettierCSS(getLayer('shortcuts')!)
+      expect(css).toEqual(
+`/* layer: shortcuts */
+.model-code-${color} {
+  position: relative;
+  min-width: 18rem;
+  overflow: hidden;
+  overflow-x: auto;
+  border-radius: 1rem;
+  --un-bg-opacity: 1;
+  background-color: rgba(${hexToRgb(color, 'DEFAULT')?.r}, ${hexToRgb(color, 'DEFAULT')?.g}, ${hexToRgb(color, 'DEFAULT')?.b}, var(--un-bg-opacity));
+  padding-top: 1.25rem;
+  padding-bottom: 1.25rem;
+  --un-text-opacity: 1;
+  ${
+    color === 'light' ?
+    'color: rgba(0, 0, 0, var(--un-text-opacity));'
+    :'color: rgba(255, 255, 255, var(--un-text-opacity));'
+  }
+}
+.model-code-${color}::before {
+  margin-bottom: 1rem;
+  display: block;
+  height: 0.75rem;
+  width: 0.75rem;
+  border-radius: 9999px;
+  opacity: 0.3;
+  --un-shadow: 1.4em 0, 2.8em 0, 4.2em 0;
+  box-shadow: var(--un-ring-offset-shadow), var(--un-ring-shadow),
+    var(--un-shadow);
+  content: '';
+}
+`)
+    })
+  }
+})
