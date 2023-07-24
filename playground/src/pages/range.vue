fd5bff0cfdc43b7e42b349b16c13ea06ae763578
elonehoo <hi@elonehoo.me>
2023-07-24 21:53:50 +0800

    fix: update range class playground

--- playground/src/pages/range.vue
+++ playground/src/pages/range.vue
@@ -7,6 +7,9 @@
     <input type="range" min="0" max="100" class="range-green range-xs max-w-xs">
   </div>
   <div>
-    <input type="range" min="0" max="100" class="range-dark range-lg max-w-xs">
+    <input type="range" min="0" max="100" class="range-green max-w-xs">
+  </div>
+  <div>
+    <input type="range" min="0" max="100" class="range-green range-lg max-w-xs">
   </div>
 </template>
