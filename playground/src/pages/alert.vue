a43a2e3f5dcc4aa230b8d143909966c30d6c77b6
elonehoo <hi@elonehoo.me>
2023-07-25 15:13:44 +0800

    fix: update playground class

--- playground/src/pages/alert.vue
+++ playground/src/pages/alert.vue
@@ -11,31 +11,3 @@
   </div>
   </div>
 </template>
-
-<style scoped>
-.alert {
-  display: grid;
-  width: 100%;
-  grid-auto-flow: row;
-  align-content: flex-start;
-  align-items: center;
-  justify-items: center;
-  gap: 1rem;
-  text-align: center;
-  border-width: 1px;
-  border-color: #191e24;
-  padding: 1rem;
-  color: #a6adba;
-  border-radius: 1rem;
-  background-color: #191e24
-}
-
-@media(min-width: 640px) {
-  .alert {
-      grid-auto-flow:column;
-      grid-template-columns: auto minmax(auto,1fr);
-      justify-items: start;
-      text-align: left
-  }
-}
-</style>
