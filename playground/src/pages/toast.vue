cb2b421060b2cea10f5f6f0a4a148fc2257333d2
elonehoo <hi@elonehoo.me>
2023-07-25 17:57:08 +0800

    feat: add example

--- playground/src/pages/toast.vue
+++ playground/src/pages/toast.vue
@@ -12,7 +12,31 @@
       </div>
     </div>
   </div>
+  <div toast="~ top">
+    <div class="alert-green">
+      <div class="i-carbon-information" />
+      <div>
+        <h3 class="font-bold">New message!</h3>
+        <div class="text-xs">You have 1 unread message</div>
+      </div>
+    </div>
+  </div>
+  <div toast="~ top start">
+    <div class="alert-green">
+      <div class="i-carbon-information" />
+      <div>
+        <h3 class="font-bold">New message!</h3>
+        <div class="text-xs">You have 1 unread message</div>
+      </div>
+    </div>
+  </div>
+  <div toast="~ start">
+    <div class="alert-green">
+      <div class="i-carbon-information" />
+      <div>
+        <h3 class="font-bold">New message!</h3>
+        <div class="text-xs">You have 1 unread message</div>
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-</style>
