ff8bc6082c4328e2c747ad0ec87e9f82e7005ccc
elonehoo <hi@elonehoo.me>
2023-07-31 20:24:05 +0800

    docs: add github link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   lang: 'en-US',
   title: '@elonehoo/ui',
+  themeConfig: {
+    socialLinks: [
+      {icon: 'github', link: 'https://github.com/elonehoo/ui'},
+    ]
+  }
 })
